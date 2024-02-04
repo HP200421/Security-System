98152eee68ae192703b00e86333093c3cb6f9ba1
Haridas Pawar <haridaspawar0203@gmail.com>
2024-02-04 12:39:40 +0530

    Declarations: Added variables and started with actual logic for the program

--- System.cpp
+++ System.cpp
@@ -7,5 +7,28 @@ using namespace std;
 
 int main()
 {
+    int choice, i=0;
+    string text, old, password0, password1, password2, pass, name, age, user, word, word1;
+    string creds[2], cp[2];
+
+    cout<<"        Security System "<<"/n";
+
+    cout<<"_______________________________"<<"/n";
+    cout<<"|                             |"<<"/n";
+    cout<<"|        1. Register          |"<<"/n";
+    cout<<"|        2. Login             |"<<"/n";
+    cout<<"|        3. Change Password   |"<<"/n";
+    cout<<"|        4. End Program       |"<<"/n";
+    cout<<"|_____________________________|"<<"/n"<<"/n"; 
+
+    do
+    {
+        cout<<"/n"<<"/n";
+
+        cout<<"Enter your choice: "<<"/n";
+        cin>>choice;
+
+        
+    }while(choice != 4);
     return 0;
 }
